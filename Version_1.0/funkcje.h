d221fddb40a4762e92cc4076702e4635ced98e11
kasiagrygorowicz <61696629+kasiagrygorowicz@users.noreply.github.com>
2020-03-20 14:54:38 +0100

    Update funkcje.h

--- Version_1.0/funkcje.h
+++ Version_1.0/funkcje.h
@@ -19,7 +19,7 @@ void generuj_jedna_plansze(int wiersze,int kolumny,int array[wiersze][kolumny],i
 void generuj_plik(int h,int wiersze,int kolumny,int array[][kolumny]);
 void generacja_cyklu(int w,int k,int t1[][k],int t2[][k],int n);
 void write_png_file(char* file_name,int w,int k);
-char* file_name(int h);
+void file_name(char *t,int h);
 void process_file(int w,int k, int array[][k]);
 
 #endif /* funkcje_h */
